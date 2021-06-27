4e78910d3647e43c128591a5e03f9bb8501aa905
Luc <lucborris@gmail.com>
2021-06-27 10:39:51 -0400

    re-enter createStyles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,24 @@
-import { Drawer, List, ListItem, ListItemText } from "@material-ui/core";
+import {
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+	Divider,
+} from "@material-ui/core";
 import Image from "next/dist/client/image";
 import styles from "../styles/SideBar.module.scss";
 import { ReactNode } from "react";
 
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		toolbar: theme.mixins.toolbar,
+	})
+);
+
 export function SideBar({ children }: { children: ReactNode }) {
+	const classes = useStyles();
 	return (
 		<>
 			<Drawer
@@ -14,7 +29,6 @@ export function SideBar({ children }: { children: ReactNode }) {
 				}}
 				anchor="left"
 			>
-				<div className={styles.toolbar} />
 				<Image
 					alt="Utah"
 					src="/Utah.jpg"
@@ -22,7 +36,8 @@ export function SideBar({ children }: { children: ReactNode }) {
 					objectFit="cover"
 					quality={100}
 				/>
-
+				<div className={classes.toolbar} />
+				<Divider />
 				<List>
 					{["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
 						<ListItem button key={text}>
