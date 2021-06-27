900dfba9ded750cb734795e47f127498b0902bea
Luc <lucborris@gmail.com>
2021-06-27 10:20:26 -0400

    fix server rendering issue with material-ui, change from createStyle to modules, better DX

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,50 +1,20 @@
 import { Drawer, List, ListItem, ListItemText } from "@material-ui/core";
 import Image from "next/dist/client/image";
-
-import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import styles from "../styles/SideBar.module.scss";
 import { ReactNode } from "react";
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			display: "flex",
-		},
-		appBar: {
-			width: `calc(100% - ${drawerWidth}px)`,
-			marginLeft: drawerWidth,
-		},
-		drawer: {
-			width: drawerWidth,
-			flexShrink: 0,
-		},
-		drawerPaper: {
-			width: drawerWidth,
-		},
-		// necessary for content to be below app bar
-		toolbar: theme.mixins.toolbar,
-		content: {
-			flexGrow: 1,
-			backgroundColor: theme.palette.background.default,
-			padding: theme.spacing(3),
-		},
-	})
-);
-
 export function SideBar({ children }: { children: ReactNode }) {
-	const classes = useStyles();
 	return (
 		<>
 			<Drawer
-				className={classes.drawer}
+				className={styles.drawer}
 				variant="permanent"
 				classes={{
-					paper: classes.drawerPaper,
+					paper: styles.drawerPaper,
 				}}
 				anchor="left"
 			>
-				<div className={classes.toolbar} />
+				<div className={styles.toolbar} />
 				<Image
 					alt="Utah"
 					src="/Utah.jpg"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CssBaseline } from "@material-ui/core";
 import { SideBar } from "../components/Sidebar";
