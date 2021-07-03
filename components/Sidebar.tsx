c9fc3776cc1e82e9c4d4f3966f99d829063269f3
Luc <lucborris@gmail.com>
2021-07-03 12:10:40 -0400

    add css to card

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,6 +10,7 @@ import Image from "next/dist/client/image";
 import { ReactNode } from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
+import Home from "../styles/Home.module.scss";
 
 const drawerWidth = 240;
 
@@ -43,53 +44,55 @@ export function SideBar({ children }: { children: ReactNode }) {
 	const classes = useStyles();
 	return (
 		<>
-			<Drawer
-				className={classes.drawer}
-				variant="permanent"
-				classes={{
-					paper: classes.drawerPaper,
-				}}
-				anchor="left"
-			>
-				<Image
-					alt="Utah"
-					src="/Utah.jpg"
-					layout="fill"
-					objectFit="cover"
-					quality={100}
-				/>
-				<div className={classes.toolbar} />
-				<Divider />
-				<List>
-					{[
-						{ province: "Newfoundland and Labrador", code: "nl" },
-						{ province: "Nova Scotia", code: "ns" },
-						{ province: "Prince Edward Island", code: "pei" },
-						{ province: "New Brunswick", code: "nb" },
-						{ province: "Quebec", code: "qc" },
-						{ province: "Ontario", code: "on" },
-						{ province: "Manitoba", code: "mb" },
-						{ province: "Saskatchewan", code: "sk" },
-						{ province: "Alberta", code: "ab" },
-						{ province: "British Columbia", code: "bc" },
-						{ province: "Yukon", code: "yk" },
-						{ province: "Northwest Territories", code: "nwt" },
-						{ province: "Nunavut", code: "nu" },
-					].map(({ province, code }, index) => (
-						<ListItem
-							button
-							key={code}
-							selected={index === selected}
-							onClick={() => setSelected(index)}
-						>
-							<Link href={`/provinces/${code}`}>
-								<ListItemText primary={province} />
-							</Link>
-						</ListItem>
-					))}
-				</List>
-			</Drawer>
-			{children}
+			<nav id="sidebar">
+				<Drawer
+					className={classes.drawer}
+					variant="permanent"
+					classes={{
+						paper: classes.drawerPaper,
+					}}
+					anchor="left"
+				>
+					<Image
+						alt="Utah"
+						src="/Utah.jpg"
+						layout="fill"
+						objectFit="cover"
+						quality={100}
+					/>
+					<div className={classes.toolbar} />
+					<Divider />
+					<List>
+						{[
+							{ province: "Newfoundland and Labrador", code: "nl" },
+							{ province: "Nova Scotia", code: "ns" },
+							{ province: "Prince Edward Island", code: "pei" },
+							{ province: "New Brunswick", code: "nb" },
+							{ province: "Quebec", code: "qc" },
+							{ province: "Ontario", code: "on" },
+							{ province: "Manitoba", code: "mb" },
+							{ province: "Saskatchewan", code: "sk" },
+							{ province: "Alberta", code: "ab" },
+							{ province: "British Columbia", code: "bc" },
+							{ province: "Yukon", code: "yk" },
+							{ province: "Northwest Territories", code: "nwt" },
+							{ province: "Nunavut", code: "nu" },
+						].map(({ province, code }, index) => (
+							<ListItem
+								button
+								key={code}
+								selected={index === selected}
+								onClick={() => setSelected(index)}
+							>
+								<Link href={`/provinces/${code}`}>
+									<ListItemText primary={province} />
+								</Link>
+							</ListItem>
+						))}
+					</List>
+				</Drawer>
+			</nav>
+			<main className={Home.main}>{children}</main>
 		</>
 	);
 }
--- pages/provinces/[province].tsx
+++ pages/provinces/[province].tsx
@@ -2,11 +2,64 @@ import { GetStaticPropsContext } from "next";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { useRouter } from "next/router";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+	root: {
+		minWidth: 275,
+	},
+	bullet: {
+		display: "inline-block",
+		margin: "0 2px",
+		transform: "scale(0.8)",
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
+});
+
 export default function Province({ props }: AppProps) {
 	const router = useRouter();
 	const { province } = router.query;
 
-	return <p>{province}</p>;
+	const classes = useStyles();
+	const bull = <span className={classes.bullet}>•</span>;
+
+	return (
+		<Card className={classes.root}>
+			<CardContent>
+				<Typography
+					className={classes.title}
+					color="textSecondary"
+					gutterBottom
+				>
+					Word of the Day
+				</Typography>
+				<Typography variant="h5" component="h2">
+					be{bull}nev{bull}o{bull}lent
+				</Typography>
+				<Typography className={classes.pos} color="textSecondary">
+					adjective
+				</Typography>
+				<Typography variant="body2" component="p">
+					well meaning and kindly.
+					<br />
+					{'"a benevolent smile"'}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size="small">Learn More</Button>
+			</CardActions>
+		</Card>
+	);
 }
 // need to find half decent api (one api has no vaccines, the other has terrible query params, where province field is a mix of province name or abbreviation), I could map the urls using my own api, will lo
 export async function getStaticPaths() {
