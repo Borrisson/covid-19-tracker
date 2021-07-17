452da65dc64c93765c982d79bc5f1bd158748e3d
Luc <lucborris@gmail.com>
2021-07-17 11:37:28 -0400

    parse data area chart

--- pages/provinces/[province].tsx
+++ pages/provinces/[province].tsx
@@ -1,6 +1,6 @@
 import { GetStaticPropsContext } from "next";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-
+import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -13,6 +13,7 @@ import {
 	YAxis,
 	Area,
 	Tooltip,
+	Legend,
 } from "recharts";
 
 const useStyles = makeStyles({
@@ -32,34 +33,68 @@ const useStyles = makeStyles({
 	},
 });
 
-export default function Province({ province }: AppProps) {
+const getFull = ({ data }) => data["all_ages"].full;
+const getOne = ({ data }) => data["all_ages"].atleast1;
+
+export default function Province({ province, data }: AppProps) {
 	const classes = useStyles();
-	console.log(province);
+	const [opacity, setOpacity] = useState({ getFull: 1, getOne: 1 });
+
+	const setFocus = (dataKey, newOpacity) => {
+		Object.keys(opacity).forEach((key) => {
+			if (key !== dataKey.name) {
+				setOpacity({ ...opacity, [key]: newOpacity });
+			}
+		});
+	};
+
 	return (
 		<Card className={classes.root}>
 			<CardContent>
-				<AreaChart
-					width={500}
-					height={300}
-					data={province.data}
-					margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
-				>
-					<XAxis dataKey="date" />
-					<YAxis />
-					<Area
-						type="monotone"
-						dataKey="total vaccinations"
-						stroke="#8884d8"
-						fillOpacity={1}
-					/>
-					<Area
-						type="monotone"
-						dataKey="change vaccinations"
-						stroke="#82ca9d"
-						fillOpacity={1}
-					/>
-					<Tooltip />
-				</AreaChart>
+				<ResponsiveContainer height={300} width={800}>
+					<AreaChart
+						data={data}
+						margin={{ top: 20, right: 30, left: 10, bottom: 0 }}
+					>
+						<defs>
+							<linearGradient id="colorFull" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+								<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+							</linearGradient>
+							<linearGradient id="colorOne" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+								<stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+							</linearGradient>
+						</defs>
+
+						<XAxis dataKey="date" />
+						<YAxis />
+
+						<Area
+							name="# of people (1 dose)"
+							type="monotone"
+							dataKey={getOne}
+							stroke="#82ca9d"
+							fillOpacity={opacity.getOne}
+							fill="url(#colorOne)"
+						/>
+						<Area
+							name="# of people (2 doses)"
+							type="monotone"
+							dataKey={getFull}
+							stroke="#8884d8"
+							fillOpacity={opacity.getFull}
+							fill="url(#colorFull)"
+						/>
+						<Tooltip />
+						<Legend
+							verticalAlign="top"
+							height={30}
+							onMouseOver={({ dataKey }) => setFocus(dataKey, 0.5)}
+							onMouseLeave={(event) => setFocus(event.dataKey, 1)}
+						/>
+					</AreaChart>
+				</ResponsiveContainer>
 			</CardContent>
 			<CardActions>
 				<Button size="small">Learn More</Button>
@@ -85,7 +120,9 @@ export async function getStaticProps({ params }: GetStaticPropsContext) {
 	const res = await fetch(
 		`https://api.covid19tracker.ca/vaccines/age-groups/province/${params?.province}`
 	);
-	let province = await res.json();
+
+	const province = await res.json();
+
 	const data = province.data.map(
 		({ date, data }: { date: string; data: string }) => {
 			data = JSON.parse(data);
