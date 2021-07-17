c5a5e1eedd942207fddd1d9fff548d871e41d3fd
Luc <lucborris@gmail.com>
2021-07-16 20:58:08 -0400

    fix parse issue

--- pages/provinces/[province].tsx
+++ pages/provinces/[province].tsx
@@ -6,7 +6,14 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-import { AreaChart, ResponsiveContainer, XAxis } from "recharts";
+import {
+	AreaChart,
+	ResponsiveContainer,
+	XAxis,
+	YAxis,
+	Area,
+	Tooltip,
+} from "recharts";
 
 const useStyles = makeStyles({
 	root: {
@@ -27,16 +34,32 @@ const useStyles = makeStyles({
 
 export default function Province({ province }: AppProps) {
 	const classes = useStyles();
-	const bull = <span className={classes.bullet}>•</span>;
-
+	console.log(province);
 	return (
 		<Card className={classes.root}>
 			<CardContent>
-				<ResponsiveContainer>
-					<AreaChart>
-						<XAxis dataKey=""></XAxis>
-					</AreaChart>
-				</ResponsiveContainer>
+				<AreaChart
+					width={500}
+					height={300}
+					data={province.data}
+					margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+				>
+					<XAxis dataKey="date" />
+					<YAxis />
+					<Area
+						type="monotone"
+						dataKey="total vaccinations"
+						stroke="#8884d8"
+						fillOpacity={1}
+					/>
+					<Area
+						type="monotone"
+						dataKey="change vaccinations"
+						stroke="#82ca9d"
+						fillOpacity={1}
+					/>
+					<Tooltip />
+				</AreaChart>
 			</CardContent>
 			<CardActions>
 				<Button size="small">Learn More</Button>
@@ -60,11 +83,16 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: GetStaticPropsContext) {
 	const res = await fetch(
-		`https://api.covid19tracker.ca/reports/province/${params?.province}`
+		`https://api.covid19tracker.ca/vaccines/age-groups/province/${params?.province}`
 	);
-	const province = await res.json();
-
+	let province = await res.json();
+	const data = province.data.map(
+		({ date, data }: { date: string; data: string }) => {
+			data = JSON.parse(data);
+			return { date, data };
+		}
+	);
 	return {
-		props: { province },
+		props: { ...province, data },
 	};
 }
